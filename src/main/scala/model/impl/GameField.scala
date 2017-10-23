314fac24c41f46802e85f5616c70376520ef0b86
Marcel Luz <luz.marcel@ymail.com>
2017-10-23 18:27:32 +0200

    use ListBuffer

--- src/main/scala/model/impl/GameField.scala
+++ src/main/scala/model/impl/GameField.scala
@@ -2,14 +2,13 @@ package model.impl
 
 import util.Point
 
-import scala.collection.mutable.ArrayBuffer
-
+import scala.collection.mutable.ListBuffer
 
 case class GameField(size: Int) {
   val MIN_POS = 0
   val MAX_POS = size - 1
 
-  var gameField: ArrayBuffer[Field] = new ArrayBuffer[Field](64)
+  var gameField: ListBuffer[Field] = new ListBuffer[Field]
   var whiteKing: King = King(Point(4, 0), 'w')
   var blackKing: King = King(Point(4, 7), 'b')
   var whiteQueen: Queen = Queen(Point(3, 0), 'w')
@@ -19,7 +18,7 @@ case class GameField(size: Int) {
   initFigures()
 
   private def initialGameField(): Unit = {
-    gameField = new ArrayBuffer[Field](64)
+    gameField = new ListBuffer[Field]
     for {
       y <- MIN_POS until size
       x <- MIN_POS until size
@@ -28,26 +27,34 @@ case class GameField(size: Int) {
     }
   }
 
-  private def initFigures():Unit ={
+  private def initFigures(): Unit = {
     initWhiteFigures()
     initBlackFigures()
   }
 
-  private def initWhiteFigures():Unit ={
-    initPawn(48,'w')
+  private def initWhiteFigures(): Unit = {
+    initPawn(48, 'w')
 
   }
-  private def initBlackFigures():Unit ={
-
-    initPawn(8,'b')
+  private def initBlackFigures(): Unit = {
+    initPawn(8, 'b')
   }
 
-  private def initPawn(offset:Int,color: Char):Unit ={
-    for(i <- 0 until 8){
-      val currentPoint = gameField(i+offset).point
-      gameField.update(offset+i,Field(currentPoint,Some(Pawn(currentPoint,color))))
-      //gameField.updated(Field(currentPoint,Some(Pawn(currentPoint,color))))
+  private def initPawn(offset: Int, color: Char): Unit = {
+    for (i <- 0 until 8) {
+      val currentPoint = gameField(i + offset).point
+      gameField.update(offset + i, Field(currentPoint, Some(Pawn(currentPoint, color))))
     }
   }
 
+  override def toString: String = {
+    var sb: String = ""
+
+    sb += "\n |  a  b  c  d  e  f  g  h  |"
+    sb += "\n-+--------------------------+"
+
+
+    sb += "\n-+--------------------------+"
+    sb
+  }
 }
